ecf1b47cd579b67a260a0427dd9ba2697cf5a51e
Melanienurazizah <143985254+Melanienurazizah@users.noreply.github.com>
2024-12-17 09:41:10 +0700

    Add files via upload

--- pages/index.vue
+++ pages/index.vue
@@ -1,62 +1,59 @@
 <template>
-    <div class="container-fluid">
-        <div class="row justify-content-around align-items-center" style="height: 80vh;">
-            <div class="col-sm-4">
-                <NuxtLink to="/point">
-                    <div class="card shadow bg1">
-                        <div class="card-body">
-                            <h5 class="card-title">MEMBERIKAN POINT </h5>
-                        </div>
-                    </div>
-                </NuxtLink>
-            </div>
-            <div class="col-sm-4">
-                <NuxtLink to="/riwayat">
-                    <div class="card bg2 shadow">
-                        <div class="card-body">
-                            <h5 class="card-title" style="opacity: none;">MELIHAT RIWAYAT POINT</h5>
-                        </div>
-                    </div>
-                </NuxtLink>
-            </div>
-            <div class="col-sm-4">
-                <NuxtLink to="/pelanggaran">
-                    <div class="card bg2 shadow">
-                        <div class="card-body">
-                            <h5 class="card-title" style="opacity: none;">MELIHAT PELANGGARAN</h5>
-                        </div>
-                    </div>
-                </NuxtLink>
-            </div>
-        </div>
+  <div class="container-fluid d-flex justify-content-center align-items-center " style="height: 80vh;">
+    <div class="card shadow" style="width: 30rem; background-color: #FFFFFF;">
+      <div class="card-body">
+        <h5 class="card-title text-center">LOGIN</h5>
+        <form @submit.prevent="handleLogin">
+          <input v-model="email" type="text" class="rounded-5 mb-5 ps-3" placeholder="Username"
+            style="width:28rem; height:3rem;background-color: #FFFFFF; ">
+          <input v-model="password" type="password" class="rounded-5 mb-5 ps-3" placeholder="Password"
+            style="width:28rem; height:3rem;background-color: #FFFFFF; ">
+          <div class="row justify-content-center">
+            <input type="submit" class="btn rounded-5"
+              style="width:8rem; background-color: #FFFFFF;" value="Login">
+          </div>
+          <!-- <i @click="togglepasswordvisibility" :class="ispasswordvisible ? 'bi bi-eye-fill' : 'bi  bi-eye-slash-fill'"
+            class="position-absolute top-50 end-0 translate-middle-y me-3" style="cursor: pointer;"></i> -->
+        </form>
+      </div>
     </div>
+  </div>
+
 </template>
 
-<style scoped>
-.card-body {
-    height: 10rem;
+<script setup>
+const client = useSupabaseClient()
+const email = ref("");
+const password = ref("");
+// const ispasswordvisible = ref(false)
+const errorMessage = ref("")
+
+// function togglepasswordvisibility() {
+//   ispasswordvisible.value = !ispasswordvisible.value
+// }
+
+async function handleLogin() {
+  const { data, error } = await client.auth.signInWithPassword({
+    email: email.value,
+    password: password.value
+  })
+
+  if (error) {
+    if (error.message.includes("invalid email")) {
+      errorMessage.value = "Email yang anda masukan salah"
+    } else if (error.message.includes("incorrect password"))
+      errorMessage.value = "Password yang anda masukan salah"
+  } else {
+    errorMessage.value = "Periksa lebih teliti !!!"
+  }
+  setTimeout(() => {
+    errorMessage.value = ""
+  }, 1000)
+
+  if (error) throw error
+  navigateTo('/menu')
 }
 
-.card {
-    position: relative;
-}
 
-.card .card-title {
-    position: relative;
-    top: 10;
-    left: 10px;
-    font-size: 40px;
-}
 
-.bg1 {
-    background-image: url("/assets/img/guru.png");
-    background-size: cover;
-
-}
-
-.bg2 {
-    background-image: url("/assets/img/siswa.png");
-    background-size: cover;
-
-}
-</style>
+</script>
---
+++ pages/menu.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="container-fluid">
+        <div class="row justify-content-around align-items-center" style="height: 80vh;">
+            <div class="col-sm-4">
+                <NuxtLink to="/point">
+                    <div class="card shadow bg1">
+                        <div class="card-body">
+                            <h5 class="card-title">MEMBERIKAN POINT </h5>
+                        </div>
+                    </div>
+                </NuxtLink>
+            </div>
+            <div class="col-sm-4">
+                <NuxtLink to="/riwayat">
+                    <div class="card bg2 shadow">
+                        <div class="card-body">
+                            <h5 class="card-title" style="opacity: none;">MELIHAT RIWAYAT POINT</h5>
+                        </div>
+                    </div>
+                </NuxtLink>
+            </div>
+            <div class="col-sm-4">
+                <NuxtLink to="/pelanggaran">
+                    <div class="card bg2 shadow">
+                        <div class="card-body">
+                            <h5 class="card-title" style="opacity: none;">MELIHAT PELANGGARAN</h5>
+                        </div>
+                    </div>
+                </NuxtLink>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.card-body {
+    height: 10rem;
+}
+
+.card {
+    position: relative;
+}
+
+.card .card-title {
+    position: relative;
+    top: 10;
+    left: 10px;
+    font-size: 40px;
+}
+
+.bg1 {
+    background-image: url("/assets/img/guru.png");
+    background-size: cover;
+
+}
+
+.bg2 {
+    background-image: url("/assets/img/siswa.png");
+    background-size: cover;
+
+}
+</style>
